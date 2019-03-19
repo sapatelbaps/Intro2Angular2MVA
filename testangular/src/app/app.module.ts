7a9247a653ae7465da3b38fb0577bd83c14af95a
sapatelbaps <sapatelbaps@gmail.com>
2019-03-18 23:08:11 -0700

    New project added to start code testing from video 13. Used command "ng g c nameofcomponent" to add a new component.

---
+++ testangular/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TaskComponent } from './task/task.component';
+import { OtherComponent } from './other/other.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TaskComponent,
+    OtherComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
