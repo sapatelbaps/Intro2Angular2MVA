635d5166c9ada3ac5937cb2b147d37460226817d
sapatelbaps <sapatelbaps@gmail.com>
2019-03-19 23:35:56 -0700

    Successfully read JSON file from a file from "assets" folder.

--- testangular15/src/app/app.module.ts
+++ testangular15/src/app/app.module.ts
@@ -1,6 +1,6 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskComponent } from './task/task.component';
@@ -12,7 +12,8 @@ import { TaskComponent } from './task/task.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- testangular15/src/app/task.service.ts
+++ testangular15/src/app/task.service.ts
@@ -1,9 +1,23 @@
+import { Response } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  public tasks;
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getTasks() {
+    console.log('Called getTasks');
+    const aPromise = this.http.get('assets/tasks.json')
+    //.map((res: Response) => res.json().data)
+    .map(res => res)
+    .toPromise();
+    aPromise.then(tasksFromServer => this.tasks = tasksFromServer);
+  }
 }
