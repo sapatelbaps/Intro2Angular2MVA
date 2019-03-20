69fc10a215faea6c9034f91e8b5e4c870d92dc16
sapatelbaps <sapatelbaps@gmail.com>
2019-03-19 21:52:01 -0700

    New project added for vide 15 - component and service created using commands ng g c compname ng generate service svcname

--- testangular15/src/app/app.module.ts
+++ testangular15/src/app/app.module.ts
@@ -1,17 +1,14 @@
-import { HttpClient } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskComponent } from './task/task.component';
-import { OtherComponent } from './other/other.component';
-import { observable } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskComponent,
-    OtherComponent
+    TaskComponent
   ],
   imports: [
     BrowserModule,
