142c25c5655817b941be293cac3bdc464600d866
sapatelbaps <sapatelbaps@gmail.com>
2019-03-21 00:36:58 -0700

    Added a new project through command. Task and TaskList components plus Task Service also added.
    All templates.

---
+++ TaskApp/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TaskComponent } from './task/task.component';
+import { TaskListComponent } from './task-list/task-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TaskComponent,
+    TaskListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
